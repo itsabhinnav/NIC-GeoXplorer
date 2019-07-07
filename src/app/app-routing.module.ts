6dcb6d480d38f9f45889a60eff545246761d9091
abhinavsri000 <abhinavsri000@gmail.com>
2019-07-07 13:19:47 +0530

    commit_5_dev

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ChartComponent } from './chart/chart.component' ;
+import { PieChartComponent } from './pie-chart/pie-chart.component' ;
 
-const routes: Routes = [];
+const approutes: Routes = [
+  {path: 'Chart', component: ChartComponent},
+  {path: 'pie-Chart', component: PieChartComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(approutes)],
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
+import { ChartsModule } from 'ng2-charts';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { MstTableComponent } from './mst-table/mst-table.component';
-import { MatTableModule } from '@angular/material' ;
-
+import { MatTableModule } from '@angular/material';
+import { ChartComponent } from './chart/chart.component';
+import { PieChartComponent } from './pie-chart/pie-chart.component' ;
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    MstTableComponent
+    MstTableComponent,
+    ChartComponent,
+    PieChartComponent
   ],
   imports: [
     BrowserModule,
+    ChartsModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     MatTableModule  
